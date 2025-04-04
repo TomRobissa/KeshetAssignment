299b677094ae15b05b6a79f5d387c7c5f9ba86d4
TomRob <TomRobissa@gmail.com>
2025-04-04 23:51:21 +0300

    add and style slider to pdf vie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,40 @@
-import { Component, signal } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
 import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 import { PDFDocumentProxy } from 'pdfjs-dist/types/src/display/api';
+import { register } from 'swiper/element/bundle';
 
+type SwiperPage = {
+  page: number;
+  isSelected: boolean;
+};
+
+register();
 
 @Component({
   selector: 'app-root',
   imports: [NgxExtendedPdfViewerModule, PdfViewerModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'keshet-assignment-receipts';
-  totalPages = signal<number>(0);
+  totalPagesArray = signal<SwiperPage[]>([]);
+  activePage = 0;
 
   onPdfLoadComplete(pdf: PDFDocumentProxy) {
-    console.log('pdf complete loaded');
-    console.dir(pdf);
-      this.totalPages.set(pdf.numPages);
+    this.totalPagesArray.set(
+      Array.from({ length: pdf.numPages }, (_, i) => {
+        return { page: i, isSelected: i === 0 };
+      })
+    );
   }
 
+  onSlideChange(event: any) {
+    const swiper = event.target.swiper; // Get Swiper instance
+    console.dir(swiper);
+    this.activePage = swiper.activeIndex + 1; // Update active page
+    console.log(`Slide changed to: ${this.activePage}`);
+  }
 }
-
