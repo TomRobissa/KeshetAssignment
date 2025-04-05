e4deb07d636232b6a00f703142dc66580fe944bc
TomRob <TomRobissa@gmail.com>
2025-04-05 18:33:21 +0300

    split view with many errors

--- src/app/payment-table/payment-table.component.ts
+++ src/app/payment-table/payment-table.component.ts
@@ -27,7 +27,16 @@ export class PaymentTableComponent {
     { id: 3,  status: 'rejected', description: 'יום צילום', supplier: 'ספק ג', amount: 500,isSelected : false },
     {  id: 4, status: 'pending', description: 'יום צילום', supplier: 'ספק ד', amount: 500,isSelected : false },
     {  id: 5, status: 'approved', description: 'יום צילום', supplier: 'ספק ה', amount: 500,isSelected : false },
-    {  id: 6, status: 'rejected', description: 'יום צילום', supplier: 'ספק ו', amount: 500,isSelected : false }
+    {  id: 6, status: 'rejected', description: 'יום צילום', supplier: 'ספק ו', amount: 500,isSelected : false },
+    { id: 7, status: 'pending', description: 'יום צילום', supplier: 'ספק א', amount: 500,isSelected : true },
+    {  id: 8, status: 'approved', description: 'יום צילום', supplier: 'ספק ב', amount: 500,isSelected : false },
+    { id: 9,  status: 'rejected', description: 'יום צילום', supplier: 'ספק ג', amount: 500,isSelected : false },
+    {  id: 10, status: 'pending', description: 'יום צילום', supplier: 'ספק ד', amount: 500,isSelected : false },
+    {  id: 11, status: 'approved', description: 'יום צילום', supplier: 'ספק ה', amount: 500,isSelected : false },
+    {  id: 12, status: 'rejected', description: 'יום צילום', supplier: 'ספק ו', amount: 500,isSelected : false },
+    { id: 13, status: 'pending', description: 'יום צילום', supplier: 'ספק א', amount: 500,isSelected : true },
+    {  id: 14, status: 'approved', description: 'יום צילום', supplier: 'ספק ב', amount: 500,isSelected : false },
+    { id: 15,  status: 'rejected', description: 'יום צילום', supplier: 'ספק ג', amount: 500,isSelected : false },
   ];
 
   getStatusIcon(payment: PaymentSelection): string {
