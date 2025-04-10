8c0f582161b094ab102372affcd10137cde48cb4
TomRob <TomRobissa@gmail.com>
2025-04-10 19:28:11 +0300

    header of app and styling. status sum styling

--- src/app/payment-status-summary/payment-status-summary.component.ts
+++ src/app/payment-status-summary/payment-status-summary.component.ts
@@ -1,23 +1,36 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { PaymentsService } from '../services/payments.service';
+import { PaymentStatus } from '../../shared/Payment';
 
 @Component({
-  selector: 'app-payment-status-summary',
-  imports: [],
-  templateUrl: './payment-status-summary.component.html',
-  styleUrl: './payment-status-summary.component.css',
+    selector: 'app-payment-status-summary',
+    imports: [],
+    templateUrl: './payment-status-summary.component.html',
+    styleUrl: './payment-status-summary.component.css',
 })
 export class PaymentStatusSummaryComponent implements OnInit {
-  paymentStatusToSum = signal<[string, number][]>([]);
+    paymentStatusToSum = signal<[string, number][]>([]);
+    paymentStatusToDisplayText: Record<string, string> = {
+        pending: 'ממתינות לאישור',
+        approved: 'אושרו',
+        rejected: 'נדחו',
+        'pending-approval': 'בתהליך',
+    };
 
-  constructor(private paymentsService: PaymentsService) {}
+    getPaymentStatusText(statusAndSum: [string, number]) {
+        return this.paymentStatusToDisplayText[statusAndSum[0]];
+    }
 
-  ngOnInit() {
-    this.paymentsService.paymentStatusToSum$.subscribe(
-      (newPaymentStatusToSum) => {
-        const paymentStatusToSumArray = Object.entries(newPaymentStatusToSum);
-        this.paymentStatusToSum.set(paymentStatusToSumArray);
-      }
-    );
-  }
+    constructor(private paymentsService: PaymentsService) {}
+
+    ngOnInit() {
+        this.paymentsService.paymentStatusToSum$.subscribe(
+            (newPaymentStatusToSum) => {
+                const paymentStatusToSumArray = Object.entries(
+                    newPaymentStatusToSum
+                );
+                this.paymentStatusToSum.set(paymentStatusToSumArray);
+            }
+        );
+    }
 }
